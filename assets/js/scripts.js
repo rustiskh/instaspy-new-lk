d87aca13d3e904fa9b79bf37d9fc344afc3c51eb
Rustam <rustam1222@gmail.com>
2023-04-06 21:25:10 +0300

    form button script (switch 'disabled') + footer

---
+++ assets/js/scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Убирает атрибут disabled в конпках в form
+    const forms = document.querySelectorAll('.form');
+
+    forms.forEach(form => {
+        const inputs = form.querySelectorAll('.general-input');
+        const button = form.querySelector('.button');
+        button.disabled = true;
+
+        inputs.forEach(input => {
+
+            input.addEventListener('input', () => {
+                if (inputs[0].value === '' || inputs[1].value === '') {
+                    button.disabled = true;
+                } else {
+                    button.disabled = false;
+                }
+            });
+        });
+    });
+});
